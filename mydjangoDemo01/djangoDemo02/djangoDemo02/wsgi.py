5a51e5fbb4326b672b877e7acbdc5e6ff5b3fa56
MosesSU <18758208937@163.com>
2020-05-10 15:13:55 +0800

    添加必要注释

--- mydjangoDemo01/djangoDemo02/djangoDemo02/wsgi.py
+++ mydjangoDemo01/djangoDemo02/djangoDemo02/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-
+# 网关对应文件
 import os
 
 from django.core.wsgi import get_wsgi_application
