df5d36eece647a4b9abd55ab73dfd82c5a1065f5
MosesSU <18758208937@163.com>
2020-05-11 01:39:40 +0800

    add two new def testRender and testRedirect

--- mydjangoDemo01/djangoDemo02/myapp02/views.py
+++ mydjangoDemo01/djangoDemo02/myapp02/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+# 测试请求转发 一个可访问应用的转发
+def testRender(request):
+    return render(request, 'testRender.html')
+
+
+# 响应重定向 新发起的请求 可以是任何可访问的url
+def testRedirect(request):
+    return redirect('http://www.baidu.com')
