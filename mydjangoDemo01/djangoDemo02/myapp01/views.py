a32c7728b072af31bcf57316af02a4b4af43f3e1
MosesSU <18758208937@163.com>
2020-05-10 16:20:14 +0800

    add real login page

--- mydjangoDemo01/djangoDemo02/myapp01/views.py
+++ mydjangoDemo01/djangoDemo02/myapp01/views.py
@@ -9,6 +9,11 @@ def test(request):
     return render(request, 'test.html')
 
 
+# add real login page
+def loginPage(request):
+    return render(request, 'login.html')
+
+
 # 要与视图函数的真正html值相同
 def topage(request, pageName):
     return render(request, pageName)
