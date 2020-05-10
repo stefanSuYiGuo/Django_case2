be4780cd513c5714dac6d658513fa412a1928a1f
MosesSU <18758208937@163.com>
2020-05-11 01:49:03 +0800

    add context in testRender def

--- mydjangoDemo01/djangoDemo02/myapp02/views.py
+++ mydjangoDemo01/djangoDemo02/myapp02/views.py
@@ -5,9 +5,12 @@ from django.shortcuts import render, redirect
 
 # 测试请求转发 一个可访问应用的转发
 def testRender(request):
-    return render(request, 'testRender.html')
+    context = dict()  # 字典类型 {key: value, key: value}
+    context['name'] = 'stefan'  # assign data
+    context['hobby'] = ['eat', 'climb']
+    return render(request, 'testRender.html', context=context)  # 只能是模板名
 
 
 # 响应重定向 新发起的请求 可以是任何可访问的url
 def testRedirect(request):
-    return redirect('http://www.baidu.com')
+    return redirect('http://www.baidu.com')  # 不能是模板名 可以是内部/外部 如： redirect('/page/login.html')
