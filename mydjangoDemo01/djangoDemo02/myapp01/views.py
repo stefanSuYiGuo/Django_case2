d422237f5f83206fde17b3d6d7df30452f3e53c6
MosesSU <18758208937@163.com>
2020-05-10 16:32:17 +0800

    get the input and judge it before login

--- mydjangoDemo01/djangoDemo02/myapp01/views.py
+++ mydjangoDemo01/djangoDemo02/myapp01/views.py
@@ -17,3 +17,14 @@ def loginPage(request):
 # 要与视图函数的真正html值相同
 def topage(request, pageName):
     return render(request, pageName)
+
+
+# add login page with functions
+def login(request):
+    # 获得客户提交的数据 get 请求方式
+    userAcc = request.GET.get('userAcc', None)  # get account
+    userPass = request.GET.get('userPass', None)  # get psd
+    if 'admin' == userAcc and userPass == '123456':
+        print('valid account')
+        return render(request, 'test.html')
+    return render(request, 'login.html')
