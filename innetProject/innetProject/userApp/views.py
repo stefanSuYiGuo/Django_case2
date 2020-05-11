b699a842d91f48df165e13f10f4e871df2ba29ed
MosesSU <18758208937@163.com>
2020-05-11 18:38:13 +0800

    get user and order & create def getOrderId()

--- innetProject/innetProject/userApp/views.py
+++ innetProject/innetProject/userApp/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import UserInfo, OrderInfo
+import time
+
 
 # Create your views here.
+# 测试数据添加
+def userRequest(request):
+    # 创建并保存对象
+    UserInfo.objects.create(userID='002',
+                            userAccount='lizzie',
+                            userPass='stefan_su',
+                            userBirth='2006-06-20',
+                            userGender='Male')
+
+    return render(request, 'test.html')
+
+
+def getOrderId():
+    time.localtime(time.time())
+
+
+def orderAdd(request):
+    # 查询这个账号密码的用户
+    user = UserInfo.objects.filter(userID='002',
+                                   userAccount='lizzie',
+                                   userPass='stefan_su',
+                                   userBirth='2006-06-20',
+                                   userGender='Male')  # 条件查询
+    OrderInfo.objects.create()
+    return None
