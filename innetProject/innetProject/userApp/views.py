fc31590e55aace5ead2b007c691d30bc73b15f29
MosesSU <18758208937@163.com>
2020-05-11 18:47:59 +0800

    转移getOrderId()到util.py

--- innetProject/innetProject/userApp/views.py
+++ innetProject/innetProject/userApp/views.py
@@ -16,13 +16,6 @@ def userRequest(request):
     return render(request, 'test.html')
 
 
-# 返回订单编号时间信息
-def getOrderId():
-    # 格式化获得当前系统时间
-    idInfo = time.strftime('%Y-%m-%d %H-%M', time.localtime(time.time()))
-    return idInfo
-
-
 def orderAdd(request):
     # 查询这个账号密码的用户
     user = UserInfo.objects.filter(userID='002',
@@ -31,7 +24,7 @@ def orderAdd(request):
                                    userBirth='2006-06-20',
                                    userGender='Male')  # 条件查询
     # 添加订单
-    OrderInfo.objects.create(orderId=(user.userID+getOrderId()),
+    OrderInfo.objects.create(orderId=(user.userID + getOrderId()),
                              orderMoney=123.5,
                              UserInfo=user)
     return None
