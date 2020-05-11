ce495fb83faaeaec0f100a36be761442283c4481
MosesSU <18758208937@163.com>
2020-05-11 18:37:32 +0800

    add an accessible path 'test/add/'

--- innetProject/innetProject/innetProject/urls.py
+++ innetProject/innetProject/innetProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from userApp import views as userViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/add/', userViews.userRequest)
 ]
