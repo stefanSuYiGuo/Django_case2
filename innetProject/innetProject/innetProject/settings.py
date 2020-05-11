348767bfd9182705d180dd09d3d0286949a789d9
MosesSU <18758208937@163.com>
2020-05-11 12:39:17 +0800

    connect with database

--- innetProject/innetProject/innetProject/settings.py
+++ innetProject/innetProject/innetProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'innetProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': 3316,
+        'NAME': 'djangob',
     }
 }
 
