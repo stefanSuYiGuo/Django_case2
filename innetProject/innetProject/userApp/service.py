70cffa13b52df1dc86dce39e6bc444c6e989b885
MosesSU <18758208937@163.com>
2020-05-12 13:36:23 +0800

    创建公共函数 判断用户账号是否存在

---
+++ innetProject/innetProject/userApp/service.py
@@ -0,0 +1,10 @@
+from .models import UserInfo, OrderInfo, Product
+
+
+# 定义函数判断用户站好是否存在
+def accountIsExit(user_account):
+    userList = UserInfo.objects.filter(userAccount=user_account)  # 根据用户账号搜索是偶有用户数据
+    if userList is not None and len(userList) > 0:
+        return True  # 代表账号不存在
+    else:
+        return False
