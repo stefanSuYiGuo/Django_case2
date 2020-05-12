179fe1d84c0db186e6a8a982bac204a1f36b5d9d
MosesSU <18758208937@163.com>
2020-05-12 14:49:37 +0800

    修改accountIsNotExit函数名 并写createUserId函数 但是这个函数有问题

--- innetProject/innetProject/userApp/service.py
+++ innetProject/innetProject/userApp/service.py
@@ -3,15 +3,19 @@ from django.db import models
 
 
 # 定义函数判断用户站好是否存在
-def accountIsExit(user_account):
+def accountIsNotExit(user_account):
     userList = UserInfo.objects.filter(userAccount=user_account)  # 根据用户账号搜索是偶有用户数据
     if userList is not None and len(userList) > 0:
-        return True  # 代表账号不存在
-    else:
         return False
+    else:
+        return True  # 代表账号不存在
 
 
 def createUserId():
+    # 获得用户编号最大值
     result = UserInfo.objects.aggregate(models.Max('userID'))
-    print('-----------' + str(result))
-    return None
+    # print('-----------' + str(result))
+    # userId = result["userID_max"] + 1
+    userId = result['userID_max'] + 1
+    # userID = int(result) + 1
+    return userId
